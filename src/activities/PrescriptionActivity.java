fcdff53ddaf16530c82909b8a6f99a13fdda55d2
Eugene Cheung <ey.cheung@outlook.com>
2014-10-13 22:29:17 -0400

    Records stuff

--- src/activities/PrescriptionActivity.java
+++ src/activities/PrescriptionActivity.java
@@ -1,38 +1,43 @@
 package activities;
 
+import global.AppState;
 import me.echeung.triage207.R;
-import me.echeung.triage207.R.id;
-import me.echeung.triage207.R.layout;
-import me.echeung.triage207.R.menu;
 import android.app.Activity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import classes.Patient;
 
 public class PrescriptionActivity extends Activity {
+
+	private Patient patient;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_prescription);
+
+		String healthCard = getIntent().getStringExtra("PATIENT");
+		patient = AppState.getPatients().get(healthCard);
 	}
 
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
-		// Inflate the menu; this adds items to the action bar if it is present.
 		getMenuInflater().inflate(R.menu.prescription, menu);
 		return true;
 	}
 
 	@Override
 	public boolean onOptionsItemSelected(MenuItem item) {
-		// Handle action bar item clicks here. The action bar will
-		// automatically handle clicks on the Home/Up button, so long
-		// as you specify a parent activity in AndroidManifest.xml.
-		int id = item.getItemId();
-		if (id == R.id.action_settings) {
+		switch (item.getItemId()) {
+		case R.id.action_accept:
+
 			return true;
+		case R.id.action_discard:
+			this.finish();
+			return true;
+		default:
+			return super.onOptionsItemSelected(item);
 		}
-		return super.onOptionsItemSelected(item);
 	}
 }
